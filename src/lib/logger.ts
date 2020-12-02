4dc9024791415f4ddf6abd5fcba99aa60bd84857
Jonah Snider <jonah@jonah.pw>
2020-12-02 01:03:39 -0800

    feat: remove file path and function name from log output

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,6 +1,6 @@
 import {Logger} from 'tslog';
 
-export const logger = new Logger({name: '@pizzafox/aoc-2020'});
+export const logger = new Logger({name: '@pizzafox/aoc-2020', displayFilePath: 'hidden', displayFunctionName: false});
 
 export function loggerScope(name: string) {
 	return logger.getChildLogger({name});
