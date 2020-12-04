38a4004d25f0988db3da87b82a542f40ae1f75de
Jonah Snider <jonah@jonah.pw>
2020-12-04 02:01:51 -0800

    refactor(day3): use a single loop over slopes

---
+++ src/lib/types.ts
@@ -0,0 +1,1 @@
+export type CoordinatePair = [x: number, y: number];
--- src/solutions/3/index.ts
+++ src/solutions/3/index.ts
@@ -1,5 +1,6 @@
 import {SolutionPair} from '../../../types/aoc';
 import {Lines} from '../../lib/fs';
+import {CoordinatePair} from '../../lib/types';
 
 enum Point {
 	Square = '.',
@@ -9,39 +10,20 @@ enum Point {
 export default function day3(rows: Lines): SolutionPair {
 	const solution: SolutionPair = [0, 1];
 
-	let [x, y]: [number, number] = [0, 0];
-
-	while (true) {
-		y += 1;
-		x += 3;
-
-		if (rows[y] === undefined) {
-			break;
-		}
-
-		if (rows[y][x] === undefined) {
-			x %= rows[y].length;
-		}
-
-		if (rows[y][x] === Point.Tree) {
-			solution[0]++;
-		}
-	}
-
-	const slopes: Array<[number, number]> = [
-		[1, 1],
+	const slopes: CoordinatePair[] = [
 		[3, 1],
+		[1, 1],
 		[5, 1],
 		[7, 1],
 		[1, 2]
 	];
 
-	const pt2counts: number[] = Array.from({length: slopes.length}, () => 0);
-	let pt2index = 0;
+	const treeCount: number[] = Array.from({length: slopes.length}, () => 0);
+	let index = 0;
 
 	for (const slope of slopes) {
-		x = 0;
-		y = 0;
+		let [x, y]: CoordinatePair = [0, 0];
+
 		while (true) {
 			x += slope[0];
 			y += slope[1];
@@ -55,16 +37,18 @@ export default function day3(rows: Lines): SolutionPair {
 			}
 
 			if (rows[y][x] === Point.Tree) {
-				pt2counts[pt2index]++;
+				treeCount[index]++;
 			}
 		}
 
-		pt2index++;
+		index++;
 	}
 
-	for (const i of pt2counts) {
-		solution[1] *= i;
+	for (const count of treeCount) {
+		solution[1] *= count;
 	}
+
+	solution[0] = treeCount[0];
 
 	return solution;
 }
